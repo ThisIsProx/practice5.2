75944a6d58f6a3099077f14963754a00b7f65684
ThisIsProx <101939838+ThisIsProx@users.noreply.github.com>
2022-04-18 12:28:45 +0200

    Initial commit

---
+++ main.js
@@ -0,0 +1,21 @@
+var auth9 = document.getElementById("auth9");
+var auth10 = document.getElementById("auth10");
+var auth11 = document.getElementById("auth11");
+var auth12 = document.getElementById("auth12");
+var auth13 = document.getElementById("auth13");
+auth9.onclick = function() {
+    alert('Той, хто в біді кидає свій народ, стає його ворогом.');
+}
+auth10.onclick = function() {
+    alert('Де немає свободи, там немає і вітчизни.')
+}
+auth11.onclick = function() {
+    alert('В своїй хаті своя й правда, І сила, і воля.')
+}
+auth12.onclick = function() {
+    alert('Вітчизна — ось і альфа, і омега!')
+}
+auth13.onclick = function() {
+    alert('Нації вмирають не від інфаркту. Спочатку їм відбирає.')
+}
+
